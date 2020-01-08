d5a229b7c1583c2eca7709132f5605b64b733a59
noname21062000 <noname21062000@gmail.com>
2020-01-09 00:06:48 +0700

    Updates

---
+++ precache-manifest.2119c6b11c677da9cc775440626301e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc280842da1a2568bb5732d32090cdb5",
+    "url": "/testChatBot/index.html"
+  },
+  {
+    "revision": "adfbc0cd9d4179eaa67e",
+    "url": "/testChatBot/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ab7d605e0fa33a6fa064",
+    "url": "/testChatBot/static/js/2.f175c293.chunk.js"
+  },
+  {
+    "revision": "99df457ec02847572eac037dc3cb0e2a",
+    "url": "/testChatBot/static/js/2.f175c293.chunk.js.LICENSE"
+  },
+  {
+    "revision": "adfbc0cd9d4179eaa67e",
+    "url": "/testChatBot/static/js/main.fe40c731.chunk.js"
+  },
+  {
+    "revision": "82b1be350bbcf6c5c916",
+    "url": "/testChatBot/static/js/runtime-main.8d5c1f78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testChatBot/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3cfe42f2ae2773f877974c3d57cf985d.js"
+  "/testChatBot/precache-manifest.2119c6b11c677da9cc775440626301e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/testChatBot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
